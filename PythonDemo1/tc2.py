ce26fb25ebefb14414af528511d8335190c0595f
sramkrishna5187@gmail.com <sramkrishna5187@gmail.com>
2023-04-25 10:57:32 +0530

    chages as per project

--- PythonDemo1/tc2.py
+++ PythonDemo1/tc2.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 global driver
-#serv_obj=Service("C:\selinium\chrome\chromedriver\chromedriver.exe")
-#driver=webdriver.Chrome(service=serv_obj)
+serv_obj=Service("C:\selinium\chrome\chromedriver\chromedriver.exe")
+driver=webdriver.Chrome(service=serv_obj)
 driver=webdriver.Chrome()
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
